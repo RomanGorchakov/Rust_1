3c9c91a173981c6230e64c358c21a429ee056d5e
RomanGorchakov <144220160+RomanGorchakov@users.noreply.github.com>
2024-12-09 10:02:00 +0000

    Rust

---
+++ Задания/Задание 2/task2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("  ------------");
+    println!("< hello world! >");
+    println!("  ------------");
+    println!("  \\   ^__^");
+    println!("   \\  (oo)\\_______");
+    println!("       (__)\\       )\\/\\");
+    println!("            ||----w |");
+    println!("            ||     ||");
+}
